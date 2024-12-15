d889520eb21dab87a9c9b5a523dc18c7f2faaee4
Lazurusaki <archines93@gmail.com>
2024-12-15 19:36:36 +0700

    GameplayBootstrap fixed

--- Assets/FinalGame/Develop/ADV_02/Game.cs
+++ Assets/FinalGame/Develop/ADV_02/Game.cs
@@ -1,5 +1,8 @@
 using System;
 using FinalGame.Develop.ADV_02.UI;
+using FinalGame.Develop.CommonServices.DataManagement.DataProviders;
+using FinalGame.Develop.CommonServices.Wallet;
+using FinalGame.Develop.ConfigsManagement;
 using FinalGame.Develop.DI;
 using FinalGame.Develop.Gameplay;
 using static FinalGame.Develop.Gameplay.GameResults;
@@ -14,14 +17,12 @@ namespace FinalGame.Develop.ADV_02
         private readonly ICondition _winCondition;
         private readonly ICondition _looseCondition;
 
-        public event Action <GameResults> GameOver;
+        public event Action<GameResults> GameOver;
 
         public Game(DIContainer container, ICondition winCondition, ICondition looseCondition)
         {
             _container = container;
-            _gameMode= _container.Resolve<GameModeHandler>().GameMode;
-            
-            
+            _gameMode = _container.Resolve<GameModeHandler>().GameMode;
 
             _winCondition = winCondition;
             _looseCondition = looseCondition;
@@ -30,15 +31,17 @@ namespace FinalGame.Develop.ADV_02
         private void OnWin()
         {
             Stop();
+            UpdatePlayerData(Win);
             GameOver?.Invoke(Win);
         }
 
         private void OnLoose()
         {
             Stop();
+            UpdatePlayerData(Loose);
             GameOver?.Invoke(Loose);
         }
-        
+
         public void Start()
         {
             _winCondition.Completed += OnWin;
@@ -46,13 +49,52 @@ namespace FinalGame.Develop.ADV_02
             _winCondition.Start();
             _looseCondition.Start();
         }
-        
+
         private void Stop()
         {
             _winCondition.Completed -= OnWin;
             _looseCondition.Completed -= OnLoose;
             _winCondition.Reset();
             _looseCondition.Reset();
+        }
+
+        private void UpdatePlayerData(GameResults gameResult)
+        {
+            _container.Resolve<GameResultsStatsService>().Add(gameResult);
+    
+            var rewardData = _container.Resolve<ConfigsProviderService>().GameRewardsConfig.GetReward(gameResult);
+        
+            switch (gameResult)
+            {
+                case GameResults.Win:
+                    HandleWin(rewardData);
+                    break;
+                case GameResults.Loose:
+                    HandleLoose(rewardData);
+                    break;
+                default:
+                    throw new ArgumentException($"Unexpected game result - {gameResult}");
+            }
+            
+            SavePlayerData();
+        }
+
+        private void HandleWin(Currency rewardData)
+        {
+            _container.Resolve<WalletService>().Add(rewardData.CurrencyType, rewardData.Value);
+        }
+        
+        private void HandleLoose(Currency rewardData)
+        {
+            var walletService = _container.Resolve<WalletService>();
+            
+            if (walletService.HasEnough(rewardData.CurrencyType,rewardData.Value ))
+                walletService.Spend(rewardData.CurrencyType, rewardData.Value);
+        }
+
+        private void SavePlayerData()
+        {
+            _container.Resolve<PlayerDataProvider>().Save();
         }
     }
 }
--- Assets/FinalGame/Develop/Gameplay/Infrastructure/GameplayBootstrap.cs
+++ Assets/FinalGame/Develop/Gameplay/Infrastructure/GameplayBootstrap.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using FinalGame.Develop.ADV_02;
 using FinalGame.Develop.ADV_02.UI;
 using FinalGame.Develop.CommonServices.AssetsManagement;
-using FinalGame.Develop.CommonServices.DataManagement.DataProviders;
 using FinalGame.Develop.CommonServices.SceneManagement;
 using FinalGame.Develop.CommonServices.Wallet;
 using FinalGame.Develop.ConfigsManagement;
@@ -33,11 +32,8 @@ namespace FinalGame.Develop.Gameplay.Infrastructure
             RegisterGameplayUIRoot();
             RegisterGuessSequencePresenter();
             RegisterGuessValidatePresenter();
-            
+
             _gameMode = _container.Resolve<GameModeFactory>().CreateGameMode(container, sceneInputArgs.GameModeName);
-            _gameResultsStatsService = _container.Resolve<GameResultsStatsService>();
-            _walletService = _container.Resolve<WalletService>();
-            _configsProviderService = _container.Resolve<ConfigsProviderService>();
 
             RegisterGameModeHandler();
 
@@ -46,7 +42,6 @@ namespace FinalGame.Develop.Gameplay.Infrastructure
             var winCondition = _container.Resolve<ConditionFactory>().CreateCondition(EndGameConditions.ValuesGuessed);
             var looseCondition = _container.Resolve<ConditionFactory>().CreateCondition(EndGameConditions.Mistake);
 
-
             _game = new Game(container, winCondition, looseCondition);
             _game.GameOver += OnGameOver;
 
@@ -54,68 +49,36 @@ namespace FinalGame.Develop.Gameplay.Infrastructure
 
             _game.Start();
         }
-        
 
         private void OnGameOver(GameResults gameResult)
         {
             _game.GameOver -= OnGameOver;
-            
-            UpdatePlayerData(gameResult);
 
-            SaveGame();
-        }
-
-        private void UpdatePlayerData(GameResults gameResult)
-        {
-            _gameResultsStatsService.Add(gameResult);
-
-            var reward = _configsProviderService.GameRewardsConfig.GetReward(gameResult);
-            
             switch (gameResult)
             {
                 case GameResults.Win:
-                    HandleWin(reward);
+                    _container.Resolve<SceneSwitcher>().ProcessSwitchSceneFor(new GameplaySceneOutputArgs(
+                        new MainMenuSceneInputArgs()));
                     break;
                 case GameResults.Loose:
-                    HandleLoose(reward);
+                    _container.Resolve<SceneSwitcher>().ProcessSwitchSceneFor(new GameplaySceneOutputArgs(
+                        new GameplaySceneInputArgs(_sceneInputArgs.GameModeName)));
                     break;
                 default:
                     throw new ArgumentException($"Unexpected game result - {gameResult}");
             }
         }
-        
-        private void HandleWin(Currency data)
-        {
-            _container.Resolve<SceneSwitcher>().ProcessSwitchSceneFor(new GameplaySceneOutputArgs(
-                new MainMenuSceneInputArgs()));
 
-            _walletService.Add(data.CurrencyType, data.Value);
-        }
-
-        private void HandleLoose(Currency data)
-        {
-            _container.Resolve<SceneSwitcher>().ProcessSwitchSceneFor(new GameplaySceneOutputArgs(
-                new GameplaySceneInputArgs(_sceneInputArgs.GameModeName)));
-            
-            if (_walletService.HasEnough(data.CurrencyType,data.Value ))
-                _walletService.Spend(data.CurrencyType, data.Value);
-        }
-
-        private void SaveGame()
-        {
-            _container.Resolve<PlayerDataProvider>().Save();
-        }
-        
         private void RegisterGameplayUIRoot()
         {
-            _container.RegisterAsSingle( c =>
+            _container.RegisterAsSingle(c =>
             {
-                var gameplayUiRootPrefab = c.Resolve<ResourcesAssetLoader>().LoadResource<GameplayUiRoot>("CommonUI/ADV_02/GameplayUiRoot");
+                var gameplayUiRootPrefab = c.Resolve<ResourcesAssetLoader>()
+                    .LoadResource<GameplayUiRoot>("CommonUI/ADV_02/GameplayUiRoot");
                 return Instantiate(gameplayUiRootPrefab);
             }).NonLazy();
         }
-        
-        
+
         private void RegisterGameModeHandler()
         {
             _container.RegisterAsSingle(c => new GameModeHandler(_gameMode));
@@ -125,19 +88,19 @@ namespace FinalGame.Develop.Gameplay.Infrastructure
         {
             _container.RegisterAsSingle(c => new GameModeFactory());
         }
-        
+
         private void RegisterConditionFactory()
         {
             _container.RegisterAsSingle(c => new ConditionFactory(_container));
         }
-        
+
         private void RegisterGuessSequencePresenter()
         {
             _container.RegisterAsSingle(c => new GuessSequencePresenter(
                 c.Resolve<GameModeHandler>().GameMode,
                 c.Resolve<GameplayUiRoot>().GeneratedSequence)).NonLazy();
         }
-        
+
         private void RegisterGuessValidatePresenter()
         {
             _container.RegisterAsSingle(c => new GuessValidatePresenter(
